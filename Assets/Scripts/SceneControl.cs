9824756f41c8b45c8b5b78ec45db0a32858071b2
mdvulfix <54022658+mdvulfix@users.noreply.github.com>
2020-01-15 14:50:16 +0300

    Start
    Start

---
+++ Assets/Scripts/SceneControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
+
+
