cf8b05ed56048e9bbef580f5d5fd64c4d9318b04
mdvulfix <mdvulfix@users.noreply.github.com>
2020-01-28 02:25:56 +0300

    28.01.2020 UI Elemants
    28.01.2020 UI Elemants

---
+++ Assets/TextBar/Title.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour
+{
+    
+    public Text title;
+    public Image box;
+    //Rect rect;
+
+    
+    private void Awake()
+    {
+        UpdateRect();
+    }
+    
+    private void Update() 
+    {
+        Vector3 titlePos = Camera.main.WorldToScreenPoint(this.transform.position);
+        //Vector3 titlePos = Camera.main.WorldToScreenPoint(new Vector3(0, Camera.main.pixelHeight, Camera.main.nearClipPlane));
+        title.transform.position = titlePos;
+        
+        //Vector2 screnPosition = new Vector2();
+        //Utility.WorldToScreenPoint(transform.position, out screnPosition);
+        //title.transform.position = screnPosition;
+    }
+
+    private void UpdateRect()
+    {
+
+        box.rectTransform.offsetMin = GetComponent<Renderer>().bounds.min*100f;
+        box.rectTransform.offsetMax = GetComponent<Renderer>().bounds.max*100f;
+
+    }
+
+}
