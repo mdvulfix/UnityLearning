70c6ed3e43660f8618ccedfcadff97376c2b3df9
mdvulfix <mdvulfix@users.noreply.github.com>
2020-01-29 01:54:20 +0300

    29.01.2020 UI Elements
    29.01.2020 UI Elements

--- Assets/TextBar/Title.cs
+++ Assets/TextBar/Title.cs
@@ -6,19 +6,48 @@ public class Title : MonoBehaviour
     
     public Text title;
     public Image box;
+    //public Camera camera;
     //Rect rect;
-
+    Renderer objRenderer;
+    Bounds objBounds;
+    Vector3 pos, posbox;
+    public Vector2 min, max;
     
     private void Awake()
     {
         //UpdateRect();
+        objRenderer = GetComponent<Renderer>();
+        objBounds = objRenderer.bounds;
+        //Messager.SendMassege(Camera.main.WorldToViewportPoint(this.transform.position).ToString());
+        pos = Camera.main.WorldToScreenPoint(objBounds.center);
+        //Messager.SendMassege(pos.ToString());
+        title.transform.position = pos;
     }
     
     private void Update() 
     {
-        Vector3 titlePos = Camera.main.WorldToScreenPoint(this.transform.position);
+        objBounds = objRenderer.bounds;
+        pos = Camera.main.WorldToScreenPoint(objBounds.center);
+        posbox = Camera.main.WorldToScreenPoint(objBounds.center);
+
+        pos.y = pos.y - Screen.height/2 + 100f;
+        pos.x = pos.x - Screen.width/2;
+        title.transform.localPosition = pos;
+        
+        posbox.y = posbox.y - Screen.height/2;
+        posbox.x = posbox.x - Screen.width/2;
+        
+        box.transform.localPosition = posbox;
+        //box.rectTransform.offsetMin = min;
+        //box.rectTransform.offsetMax = max;
+        
+        
+        
+        
+        //Vector3 titlePos = camera.WorldToScreenPoint(this.transform.position);
+        //Vector3 titlePos = new Vector3(transform.position.x, transform.position.y);
         //Vector3 titlePos = Camera.main.WorldToScreenPoint(new Vector3(0, Camera.main.pixelHeight, Camera.main.nearClipPlane));
-        title.transform.position = titlePos*0.01f;
+        //title.transform.position = Camera.main.WorldToScreenPoint(titlePos);
         
         //Vector2 screnPosition = new Vector2();
         //Utility.WorldToScreenPoint(transform.position, out screnPosition);
